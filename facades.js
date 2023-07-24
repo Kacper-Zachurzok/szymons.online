c2cf14be09c3ff784ef1cfe9967f0428f6395a02
Kacper Zachurzok <Kacper.Zachurzok@gmail.com>
2023-07-24 08:33:11 +0200

    Add facade to youtube iframe

---
+++ facades.js
@@ -0,0 +1,11 @@
+﻿const facades = document.querySelectorAll(".facade");
+facades.forEach((facade) => {
+  facade.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = facade.getAttribute("for");
+    const element = document.getElementById(id);
+    const src = element.getAttribute("facade-src");
+    element.setAttribute("src", src);
+    facade.remove();
+  });
+});
